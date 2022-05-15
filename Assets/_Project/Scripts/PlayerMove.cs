3bd1e294aac8e1397438eb6e56afdc1575981e13
ayutaz <ka1357amnbpdr@gmail.com>
2022-05-15 23:59:49 +0900

    feat: player move on inputSystem

---
+++ Assets/_Project/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        private Game _game;
+
+        private void Awake()
+        {
+            _game = new Game();
+            _game.Enable();
+        }
+
+        private void Update()
+        {
+            var move = _game.Map.Move.ReadValue<Vector2>();
+            transform.Translate(move * Time.deltaTime * 2f);
+        }
+    }
+}
