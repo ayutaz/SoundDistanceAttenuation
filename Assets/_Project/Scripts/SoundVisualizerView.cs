919154886e19d2693ceb8532f1f772a925d7f28f
ayutaz <ka1357amnbpdr@gmail.com>
2022-06-05 04:27:29 +0900

    feat: 各画像の起動時の円形配置と色のランダム化

---
+++ Assets/_Project/Scripts/SoundVisualizerView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project
+{
+    public class SoundVisualizerView : MonoBehaviour
+    {
+        [SerializeField] private List<Image> visualizerImageList;
+
+        private void Awake()
+        {
+            foreach (var (image, index) in visualizerImageList.Select((item, index) => (item, index)))
+            {
+                image.transform.rotation = Quaternion.Euler(0f, 0f, index * 360f / visualizerImageList.Count);
+                image.color = RandomColor();
+            }
+        }
+
+        private static Color32 RandomColor()
+        {
+            return new Color32(
+                (byte)Random.Range(0, 255),
+                (byte)Random.Range(0, 255),
+                (byte)Random.Range(0, 255),
+                255
+            );
+        }
+    }
+}
