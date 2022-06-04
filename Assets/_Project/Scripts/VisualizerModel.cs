a9a6fc0ef2074ab05e795f70b61020ceaa283d65
ayutaz <ka1357amnbpdr@gmail.com>
2022-06-05 05:33:02 +0900

    feat: visualiserData Model class

---
+++ Assets/_Project/Scripts/VisualizerModel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace _Project
+{
+    public class VisualizerModel
+    {
+        private float[] _visualiserData = new float[16];
+
+        public float[] GetVisualizerData()
+        {
+            return _visualiserData;
+        }
+
+        public void UpdateVisualizerData(VisualizerAudioData data)
+        {
+        }
+
+        public static float Vector2ToAngle(Vector2 vector2)
+        {
+            return Mathf.Atan2(vector2.y, vector2.x) * Mathf.Rad2Deg;
+        }
+    }
+}
