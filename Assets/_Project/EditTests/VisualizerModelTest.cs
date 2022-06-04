a028b8e71ef0f03dc696a1af1da0fc213a5ed83b
ayutaz <ka1357amnbpdr@gmail.com>
2022-06-05 06:03:08 +0900

    fix: Vector2ToAngleの計算を-180 ~ 180から0~360への変更

--- Assets/_Project/EditTests/VisualizerModelTest.cs
+++ Assets/_Project/EditTests/VisualizerModelTest.cs
@@ -7,11 +7,11 @@ public class VisualizerModelTest
     [Test]
     public void Vector2ToAngleTest()
     {
-        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(1.0f, 0.0f)), 0); // 0
-        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(0.7f, 0.7f)), 45); // 45
-        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(0.0f, 1.0f)), 90); // 90
-        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(-1.0f, 0.0f)), 180); // 180
-        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(0.0f, -1.0f)), -90); // -90
-        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(1.0f, 0.0f)), 0); // 0
+        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(1.0f, 0.0f)), 0);
+        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(0.7f, 0.7f)), 45);
+        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(0.0f, 1.0f)), 90);
+        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(-1.0f, 0.0f)), 180);
+        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(0.0f, -1.0f)), 270);
+        Assert.AreEqual(VisualizerModel.Vector2ToAngle(new Vector2(1.0f, 0.0f)), 0);
     }
 }
--- Assets/_Project/Scripts/VisualizerModel.cs
+++ Assets/_Project/Scripts/VisualizerModel.cs
@@ -4,7 +4,14 @@ namespace _Project
 {
     public class VisualizerModel
     {
-        private float[] _visualiserData = new float[16];
+        private readonly float[] _visualiserData;
+        private readonly float _minValue;
+
+        public VisualizerModel(int visualizerMaxDataCount, float minValue)
+        {
+            _visualiserData = new float[visualizerMaxDataCount];
+            this._minValue = minValue;
+        }
 
         public float[] GetVisualizerData()
         {
@@ -13,11 +20,24 @@ namespace _Project
 
         public void UpdateVisualizerData(VisualizerAudioData data)
         {
+            if (data.AudioValue <= _minValue) return;
+            var angle = Vector2ToAngle(data.AudioVector);
+            Debug.Log($"value:{data.AudioValue}, angle:{angle}");
+            _visualiserData[VisualizerDataIndex(angle)] = data.AudioValue;
+        }
+
+        private static int VisualizerDataIndex(float angle)
+        {
+            var i = Mathf.CeilToInt(360f / angle);
+            Debug.Log("i: " + i);
+            return i;
         }
 
         public static float Vector2ToAngle(Vector2 vector2)
         {
-            return Mathf.Atan2(vector2.y, vector2.x) * Mathf.Rad2Deg;
+            var angle = Mathf.Atan2(vector2.y, vector2.x) * Mathf.Rad2Deg;
+            if (0f <= angle) return angle;
+            return 360f + angle;
         }
     }
 }
