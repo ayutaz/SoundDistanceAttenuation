0442d84cd31af810c23fb33fa7e2873791893b66
ayutaz <ka1357amnbpdr@gmail.com>
2022-06-05 14:27:38 +0900

    style: remove using

--- Assets/_Project/Scripts/SoundVisualizerController.cs
+++ Assets/_Project/Scripts/SoundVisualizerController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using NaughtyAttributes;
-using UniRx;
 using UnityEngine;
 
 namespace _Project
